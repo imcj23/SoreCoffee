7f9748215ddb1cf040cac661047702b01fb29375
Cakjon <wicaksanaadisaputro@gmail.com>
2026-01-27 03:31:46 +0700

    update & modified

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import CartContext from '../context/CartContext';
+import CartContext from '../context/cartcontext';
 
 // Helper function untuk parse harga
 const parsePrice = (priceString) => {
