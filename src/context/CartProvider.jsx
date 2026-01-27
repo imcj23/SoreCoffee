6a9616ccf1fdeafec041201311fdabc198530ea9
Cakjon <wicaksanaadisaputro@gmail.com>
2026-01-27 14:44:15 +0700

    update provider

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import CartContext from './CartContext';
+import CartContext from './cartcontext';
 
 const parsePrice = (priceString) => {
   if (!priceString || typeof priceString !== 'string') return 0;
